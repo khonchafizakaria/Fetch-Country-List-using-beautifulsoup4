6fefe1363f8781beefe639f2bee9e905473292d1
KHONCHAFI Zakaria <zakaria.khonchafi@gmail.com>
2019-07-28 11:55:46 +0100

    Update views.py

--- views.py
+++ views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 from .models import Country
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import time
 import json
 
 # Create your views here.
 
+# Any Request sent to a django view requires a csrf_token, we will add @csrf_exempt decorator to allow POST request.
 @csrf_exempt
 def create(request):
+    
+    # Serialize your data
     data = json.loads(request.body)
-    print(data)
+    
+    # Save
     c = Country.objects.create(name=data["name"], alpha_2=data["alpha_2"], alpha_3=data["alpha_3"], available=data["available"])
     c.save()
+    
+    # You have to return an HttpResponse or else Django will complain. 
     return HttpResponse(status=201)
